c80e36f3d0df4158c430bf52c0eb5e8786d82223
sorvylenny <115513656+sorvylenny@users.noreply.github.com>
2023-12-18 01:04:26 -0500

    update Card Proyect and skill
    pending the footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PortafolioComponent } from './Profile/portafolio/portafolio.component';
 import { HttpClientModule } from '@angular/common/http';
+import { FooterComponent } from './Profile/footer/footer.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     PortafolioComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
